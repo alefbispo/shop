55f0489a5567e3bbe44fea2769dadb30a85c134b
alefbispo <Blacksabbath21@>
2022-06-22 22:33:03 -0300

    trabalhando as caracteristicas de exibição dos produtos

---
+++ lib/components/product_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductOverviewPage extends StatelessWidget {
   ProductOverviewPage({Key? key}) : super(key: key);
   final List<Product> loadedProducts = dummyProducts;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,17 +15,15 @@ class ProductOverviewPage extends StatelessWidget {
           child: Text('Minha Loja'),
         ),
       ),
-      body: Padding(
+      body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10),
-        ),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
       ),
     );
   }
