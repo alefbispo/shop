d66704e9594e250499dbfab9e89d23e877e5f0f0
alefbispo <Blacksabbath21@>
2022-06-24 22:26:54 -0300

    criando rotas nomeadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Product product;
     return MaterialApp(
+      routes: {
+        AppRoutes.PRODUCT_DETAIL:(context) => 
+        ProductDetalPage()
+      },
       theme: ThemeData(
           fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
