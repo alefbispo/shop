fa267cc7b0d01bd38847fbf69ba79a08251ff0aa
alefbispo <Blacksabbath21@>
2022-06-26 17:19:26 -0300

    instanciando provider na aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -13,16 +15,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (context) => const ProductDetalPage(),
-       },
-      theme: ThemeData(
-          fontFamily: 'Lato',
-          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
-              .copyWith(secondary: Colors.deepOrange)),
-      debugShowCheckedModeBanner: false,
-      home: ProductOverviewPage(),
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (context) => const ProductDetalPage(),
+         },
+        theme: ThemeData(
+            fontFamily: 'Lato',
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                .copyWith(secondary: Colors.deepOrange)),
+        debugShowCheckedModeBanner: false,
+        home: ProductOverviewPage(),
+      ),
     );
   }
 }
