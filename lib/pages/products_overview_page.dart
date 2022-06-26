e17818bffe242cd3b92588f68670a5693d4c063f
alefbispo <Blacksabbath21@>
2022-06-26 20:09:16 -0300

    selecionando apenas itens curtidos da lsits

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 
 import '../components/product_grid.dart';
+
+enum FilterOptions { Favorite, All }
 
 class ProductOverviewPage extends StatelessWidget {
   const ProductOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Center(
           child: Text('Minha Loja'),
         ),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions value) {
+              if (value == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+            initialValue: FilterOptions.Favorite,
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorite,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Todos'),
+              ),
+            ],
+          ),
+        ],
       ),
       body: ProductGrid(),
     );
