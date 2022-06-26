62d5a5ce3def17fbd0159b49badf29dd45229dc8
alefbispo <Blacksabbath21@>
2022-06-26 17:00:48 -0300

    simulado a utulização com inherted widget e volando ao estado nomral da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Product product;
     return MaterialApp(
       routes: {
         AppRoutes.PRODUCT_DETAIL: (context) => const ProductDetalPage(),
-      },
+       },
       theme: ThemeData(
           fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
