6293122179ef98298cf15e2b38639dfaaceb266c
alefbispo <Blacksabbath21@>
2022-06-27 19:28:29 -0300

    check

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -28,3 +28,29 @@ class ProductList with ChangeNotifier {
     notifyListeners();
   }
 }
+// class ProductList with ChangeNotifier {
+//   final List<Product> _items = dummyProducts;
+//   bool _showFavoritesOnly = false;
+
+//   List<Product> get items {
+//     if (_showFavoritesOnly) {
+//       return _items.where((element) => element.isFavorite).toList();
+//     }
+//     return [..._items];
+//   }
+
+//   void showFavoriteOnly() {
+//     _showFavoritesOnly = true;
+//     notifyListeners();
+//   }
+
+//   void showAll() {
+//     _showFavoritesOnly = false;
+//     notifyListeners();
+//   }
+
+//   void addProduct(Product product) {
+//     _items.add(product);
+//     notifyListeners();
+//   }
+// }
