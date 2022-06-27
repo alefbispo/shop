640fdb87340887abbc26ab658b2817d8950d1b77
alefbispo <Blacksabbath21@>
2022-06-27 20:23:29 -0300

    criando a interface de carrinho para adicionar ao projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
@@ -15,18 +15,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ProductList(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        )
+      ],
       child: MaterialApp(
         routes: {
           AppRoutes.PRODUCT_DETAIL: (context) => const ProductDetalPage(),
-         },
+        },
         theme: ThemeData(
             fontFamily: 'Lato',
             colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
                 .copyWith(secondary: Colors.deepOrange)),
         debugShowCheckedModeBanner: false,
-        home: ProductOverviewPage(),
+        home: const ProductOverviewPage(),
       ),
     );
   }
