b1922d7f70026fb41ade5968f4c65da8ae1a7ed4
alefbispo <Blacksabbath21@>
2022-06-30 22:22:49 -0300

    adicionando função de arrastar para deletar itens

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/cart_item.dart';
 
 class CartItemWidget extends StatelessWidget {
@@ -8,22 +10,44 @@ class CartItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final total = cartItem.price * cartItem.quantity;
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Padding(
-            padding: const EdgeInsets.all(2),
-            child: FittedBox(
-              child: Text(
-                cartItem.price.toStringAsFixed(2),
+    return Dismissible(
+      onDismissed: (_) {
+        Provider.of<Cart>(context, listen: false)
+            .removeItem(cartItem.productId);
+      },
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.startToEnd,
+      background: Container(
+        color: Theme.of(context).errorColor,
+        alignment: Alignment.centerLeft,
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            child: Padding(
+              padding: const EdgeInsets.all(2),
+              child: FittedBox(
+                child: Text(
+                  cartItem.price.toStringAsFixed(2),
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
           ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: R\$ $total'),
+          trailing: Text('${cartItem.quantity}x'),
         ),
-        title: Text(cartItem.name),
-        subtitle: Text('Total: R\$ $total'),
-        trailing: Text('${cartItem.quantity}x'),
       ),
     );
   }
