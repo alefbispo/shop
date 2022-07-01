06a3c720fdab0ae021dee1a7b3bf905a0cd100b8
alefbispo <Blacksabbath21@>
2022-06-30 21:28:03 -0300

    formatação de margens

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -7,6 +7,24 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    final total = cartItem.price * cartItem.quantity;
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Padding(
+            padding: const EdgeInsets.all(2),
+            child: FittedBox(
+              child: Text(
+                cartItem.price.toStringAsFixed(2),
+              ),
+            ),
+          ),
+        ),
+        title: Text(cartItem.name),
+        subtitle: Text('Total: R\$ $total'),
+        trailing: Text('${cartItem.quantity}x'),
+      ),
+    );
   }
 }
