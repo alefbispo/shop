cf4b1f7463455cb74ab4b85321672b62cbb8e6c7
alefbispo <Blacksabbath21@>
2022-07-03 21:33:39 -0300

    trabalhando em pagina de meus pedidos

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -11,6 +11,7 @@ class AppDrawer extends StatelessWidget {
         children: [
           AppBar(
             title: const Text('Bem Vindo!'),
+            automaticallyImplyLeading: false,
           ),
           const Divider(),
           ListTile(
