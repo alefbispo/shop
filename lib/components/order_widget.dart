32687dbe75f896309698f953d531767989c4fb51
alefbispo <Blacksabbath21@>
2022-07-03 22:07:27 -0300

    detalhes do pedido

--- lib/components/order_widget.dart
+++ lib/components/order_widget.dart
@@ -2,25 +2,67 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
   const OrderWidget({required this.order, Key? key}) : super(key: key);
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Text(
-            order.products[0].quantity.toString(),
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              child: Text(
+                widget.order.products[0].quantity.toString(),
+              ),
+            ),
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date),
+            ),
+            trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: const Icon(Icons.expand_more)),
           ),
-        ),
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.date),
-        ),
-        trailing:
-            IconButton(onPressed: () {}, icon: const Icon(Icons.expand_more)),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: widget.order.products.length * 35,
+              child: ListView(
+                children: widget.order.products
+                    .map((product) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              product.name,
+                              style: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${product.quantity}x R\$ ${product.price}',
+                              style: const TextStyle(
+                                  fontSize: 18, color: Colors.grey),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
+            ),
+        ],
       ),
     );
   }
