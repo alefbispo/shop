997b1894296584c44802f09306852bd759741a41
alefbispo <Blacksabbath21@>
2022-07-13 19:59:17 -0300

    nova pagina de formulario com rotas nomeadas

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -16,8 +16,16 @@ class ProductItem extends StatelessWidget {
         width: 100,
         child: Row(
           children: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.delete))
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.edit),
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.delete),
+              color: Theme.of(context).colorScheme.error,
+            )
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/product_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -38,6 +39,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.CART: (context) => const CartPage(),
           AppRoutes.ORDERS: (context) => const OrdersPage(),
           AppRoutes.PRODUCTS: (context) => const ProductsPage(),
+          AppRoutes.PRODUCT_FORM: (context) => const ProductFormPage(),
         },
         theme: ThemeData(
             fontFamily: 'Lato',
